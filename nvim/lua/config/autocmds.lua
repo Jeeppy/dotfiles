959a7c804e64fd0129edd4f4d79ba947c278b5ec
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-03-31 17:30:31 +0200

    feat (nvim): add auto create directory on save

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -24,3 +24,12 @@ vim.api.nvim_create_autocmd(
     "BufWritePre",
     { pattern = "*", command = "%s/\\s\\+$//e" }
 )
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
