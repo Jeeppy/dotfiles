4a9c665de5c8fa2d77ef3f69bc0d334be92b808c
Jean-Patrick Debaëne <jp@debaene.fr>
2026-02-25 17:28:57 +0100

    feat: add claude settings

--- zsh/.zsh/30-dev.zsh
+++ zsh/.zsh/30-dev.zsh
@@ -10,3 +10,12 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# bun completions
+[ -s "/home/jp/.bun/_bun" ] && source "/home/jp/.bun/_bun"
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
+
+alias claude-mem='/home/jp/.bun/bin/bun "/home/jp/.claude/plugins/cache/thedotmack/claude-mem/10.4.3/scripts/worker-service.cjs"'
